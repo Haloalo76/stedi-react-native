587b6c5257315cbd29e128ff69f7d52655414669
Haloalo76 <97320356+Haloalo76@users.noreply.github.com>
2022-03-12 16:06:53 -0700

    Update login.js

--- login.js
+++ login.js
@@ -1,27 +1,58 @@
 import React from 'react';
-import {StyleSheet, View, Text, Button} from 'react-native';
+import {SafeAreaView, StyleSheet, TextInput, Button} from 'react-native';
 
-export default function Login(props){
-    return(
-        <View style={styles.login}>
-            <Text>This is the Login Screen</Text>
-            <Button title="Log In" onPress={()=>props.setUserLoggedIn(true)}></Button>
-        </View>
 
-    );
+const Login = (props) => {
+  const [text, onChangeText] = React.useState(null);
+  const [number, onChangeNumber] = React.useState(null);
+
+  return (
+    <SafeAreaView>
+      <TextInput
+        style={styles.input}
+        onChangeText={onChangeText}
+        placeholder="Phone Number"
+        keyboardType="numeric"
+        value={text}
+      />
+      <Button
+        title="Send Code"
+        onPress={() => send_code(text)}
+/>
+      <TextInput
+        style={styles.input}
+        onChangeText={onChangeNumber}
+        value={number}
+        placeholder="Key Code"
+        keyboardType="numeric"
+      />
+      <Button
+      title="Log In"
+      onPress={() => Login()}
+      />
+    </SafeAreaView>
+  ); 
 }
 
 const styles = StyleSheet.create({
-    login: {
-        flexDirection: 'row',
-        width: '100%',
-        justifyContent: 'space-between',
-        backgroundColor: 'green',
-        height: '12%',
-        alignItems: 'flex-end',
-        paddingBottom: 5,
-        paddingLeft: 10,
-        paddingRight: 10,
-        
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  },
+});
+
+
+export default Login;
+
+
+const send_code = (onChangeText) => {
+  fetch('https://dev.stedi.me/twofactorlogin/'+ onChangeText, {
+    method: 'POST',
+      headers: {
+        Accept: 'application/json',
+          'Content-Type': 'application/text'
       },
-})
+  });
+}
